b5f5ea3aeb7d780483a1e62708c9582dd34acbfb
iamadityaaz <adityaprakash159@gmail.com>
2020-10-29 11:04:16 +0530

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var AWS = require('aws-sdk/');
 
 function runFun() {
-  const credentials = new AWS.CredentialProviderChain();
+  const credentials = new AWS.CredentialProviderChain(); // TODO confused with credentials
   const client = new AWS.Glacier(credentials);
   client.endpoint('https://glacier.ap-south-1.amazonaws.com/');
 
   try {
     client.deleteArchive(
-      { accountId: '', archiveId: '', vaultName: '' },
+      {
+        accountId: '', // TODO add account ID
+        archiveId:
+          'PXzNfpTPrUEmjHJ_MN7FPL65FwPNQPor1Diqn4xiADJdB5XgUvyyac8rdE5MbAWMe8fgZR64qQCbAi5Umy3zhrDIWS22xPQDv9V-KXes-TmkgwR_mnYQKSVGCWvULvoTJlCwhDsSng',
+        vaultName: 'RAW_VIDEO_FILES',
+      },
       (err, data) => {
         if (err) {
           console.log(err);
